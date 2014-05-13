594bf2d8adc67c3579048f202977cf9becafb0a9
Isay Sosa <yasnet21@gmail.com>
2014-05-13 12:09:38 -0500

    init Mercury

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount Mercury::Engine => '/'
   root to: 'pages#show', id: '1'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
